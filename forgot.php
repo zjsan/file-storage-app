54387fc1192a70be7f884b51dde16c8fa43e41c4
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-06-08 11:00:20 +0800

    Update forgot.php

--- forgot.php
+++ forgot.php
@@ -8,6 +8,9 @@
   <link rel="stylesheet" href="./css/body.css">
   <link rel="stylesheet" href="./css/login.css">
   <link rel="stylesheet" href="./css/forgot.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
 
