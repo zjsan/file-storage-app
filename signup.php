45b07a4bd718a14d36256c38543bc87423dfa750
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-06-08 11:00:35 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -32,6 +32,9 @@ session_start(); // Ensure session starts before checking messages
   <link rel="stylesheet" href="./css/body.css">
   <link rel="stylesheet" href="./css/form-signup.css">
   <link rel="stylesheet" href="./css/form-container.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+ <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+ <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
 
